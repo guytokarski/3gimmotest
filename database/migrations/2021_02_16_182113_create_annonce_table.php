eead21db2f85223139443bb3248faf96475c9a61
guytokarski <guytokarski@gmail.com>
2021-02-16 19:39:18 +0100

    create annonce table

---
+++ database/migrations/2021_02_16_182113_create_annonce_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnonceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('annonce', function (Blueprint $table) {
+            $table->id();
+            $table->string('ref_annonce', 20);
+            $table->float('prix_annonce');
+            $table->float('surface_habitable');
+            $table->integer('nombre_de_piece');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('annonce');
+    }
+}
